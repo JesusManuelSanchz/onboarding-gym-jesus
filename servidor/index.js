465627a29e879bdf6ab533f3ce52f787e2c8eb6c
JesusManuelSanchz <chuchopro76@gmail.com>
2026-08-26 11:34:14 -0600

    Compila archivo TypeScript a JavaScript

--- servidor/index.js
+++ servidor/index.js
@@ -1,21 +1,37 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const pg_1 = require("pg");
 const app = (0, express_1.default)();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.use((0, cors_1.default)());
-app.get('/api/clases', (req, res) => {
-    const clases = [
-        { id: 1, nombre: 'Crossfit', horario: '08:00 AM', instructor: 'Carlos' },
-        { id: 2, nombre: 'Yoga', horario: '10:00 AM', instructor: 'María' },
-        { id: 3, nombre: 'Boxeo', horario: '06:00 PM', instructor: 'Luis' }
-    ];
-    res.json(clases);
+app.use(express_1.default.static('public'));
+const pool = new pg_1.Pool({
+    connectionString: process.env.DATABASE_URL,
 });
+app.get('/api/clases', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const respuesta = yield pool.query('SELECT * FROM clases');
+        res.json(respuesta.rows);
+    }
+    catch (error) {
+        console.error('Error en la base de datos:', error);
+        res.status(500).json({ error: 'Error al conectar con la base de datos' });
+    }
+}));
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
